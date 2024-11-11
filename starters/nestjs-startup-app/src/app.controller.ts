39ef3d6960011ff49644fb287f6757852e91f2ab
Zaker-Mohammed_pickles <zaker.mohammed@pickles.com.au>
2024-11-11 10:04:48 +0000

    chore(repo): Playing With Config Episode 3
    Release-As

--- starters/nestjs-startup-app/src/app.controller.ts
+++ starters/nestjs-startup-app/src/app.controller.ts
@@ -1,6 +1,6 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
-import { helloWorld } from '@smart-utility/hello-world-util';
+import { helloWorld } from '@smart-utilities/hello-world-util';
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
