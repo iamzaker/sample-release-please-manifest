c856e0a4f43515d20e36fcb2fe0811b602524961
iamzaker <mzaker1234@gmail.com>
2024-11-12 10:42:32 +0530

    fix(hello-world-util): bump version to 0.0.2 (#89)
    Feature to check the bumping of a newer patch
    version
    Release-As: 0.0.2
    
    Co-authored-by: Zaker-Mohammed_pickles <zaker.mohammed@pickles.com.au>

--- packages/hello-world-util/src/hello-world.ts
+++ packages/hello-world-util/src/hello-world.ts
@@ -1,2 +1,21 @@
 
 export const helloWorld = () => 'Hello World!';
+
+export const formatDate = (date: Date, format: string): string => {
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
+    const hours = String(date.getHours()).padStart(2, '0');
+    const minutes = String(date.getMinutes()).padStart(2, '0');
+    const seconds = String(date.getSeconds()).padStart(2, '0');
+  
+    const formattedDate = format
+      .replace('YYYY', year.toString())
+      .replace('MM', month)
+      .replace('DD', day)
+      .replace('HH', hours)
+      .replace('mm', minutes)
+      .replace('ss', seconds);
+      console.log(formattedDate);
+      return formattedDate;
+  };
