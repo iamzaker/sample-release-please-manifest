58d29c70aebd19e3162eff9c9e8400b70903fee5
Zaker-Mohammed_pickles <zaker.mohammed@pickles.com.au>
2024-11-28 09:51:19 +0000

    chore(repo): update release workflow

--- packages/hello-world-util/src/hello-world.ts
+++ packages/hello-world-util/src/hello-world.ts
@@ -1,5 +1,5 @@
 
-export const helloWorld = () => 'Hello World from package!';
+export const helloWorld = () => 'Hello World from package v:0.0.4!';
 
 export const formatDate = (date: Date, format: string): string => {
     const year = date.getFullYear();
