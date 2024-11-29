2053f5bf5314a8a2d512fa3315ea2eade360f6bb
Zaker-Mohammed_pickles <zaker.mohammed@pickles.com.au>
2024-11-29 16:16:50 +0000

    fix(repo): updated input

--- packages/hello-world-util/src/hello-world.spec.ts
+++ packages/hello-world-util/src/hello-world.spec.ts
@@ -2,6 +2,6 @@ import { helloWorld } from './hello-world';
 
 describe('index', () => {
     it('should return true', () => { 
-        expect(helloWorld()).toEqual('Hello World from package with v:0.0.4!');
+        expect(helloWorld()).toEqual('Hello World from package with version:0.0.4!');
     });
  });
