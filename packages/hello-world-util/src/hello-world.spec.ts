b822d28961b52a3c3674031c8716c096418e9c6d
Zaker-Mohammed_pickles <zaker.mohammed@pickles.com.au>
2024-11-29 06:47:28 +0000

    fix(hello-world): add missing files

--- packages/hello-world-util/src/hello-world.spec.ts
+++ packages/hello-world-util/src/hello-world.spec.ts
@@ -2,6 +2,6 @@ import { helloWorld } from './hello-world';
 
 describe('index', () => {
     it('should return true', () => { 
-        expect(helloWorld()).toEqual('Hello World!');
+        expect(helloWorld()).toEqual('Hello World from package with v:0.0.4!');
     });
  });
