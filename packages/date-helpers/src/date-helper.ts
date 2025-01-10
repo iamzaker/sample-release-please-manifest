722885bebb43c10de4f1e768b754049a1a768abd
Zaker-Mohammed_pickles <zaker.mohammed@pickles.com.au>
2025-01-10 07:01:10 +0000

    fix(date-helper): build-test-stage-3

--- packages/date-helpers/src/date-helper.ts
+++ packages/date-helpers/src/date-helper.ts
@@ -1,5 +1,5 @@
 export const helloWorld = (): string => {
-  const message = 'Hello World from package version 0.0.19!';
+  const message = 'Hello World from package version 0.0.20!';
   console.log(message);
   return message;
 };
