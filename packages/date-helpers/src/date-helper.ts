8cfbab829d982f8523224e35a163dea2e8e09410
iamzaker <mzaker1234@gmail.com>
2025-01-10 12:10:20 +0530

    fix(repo): release-test-statge-2 (#138)
    * fix(repo): release-test-statge-2
    
    1) packages/date-helpers/src/date-helper.ts
    2) release-please-config.json
    
    * fix(repo)--release-test-statge-2
    
    ---------
    
    Co-authored-by: Zaker-Mohammed_pickles <zaker.mohammed@pickles.com.au>

--- packages/date-helpers/src/date-helper.ts
+++ packages/date-helpers/src/date-helper.ts
@@ -1,5 +1,5 @@
 export const helloWorld = (): string => {
-  const message = 'Hello World from package version 0.0.18 !';
+  const message = 'Hello World from package version 0.0.19!';
   console.log(message);
   return message;
 };
